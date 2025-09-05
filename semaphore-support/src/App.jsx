38cc74ee150e5b6beedce28ce582cd2897d3fbb1
Sumanth <sumanthpoojary965@gmail.com>
2025-09-05 18:14:10 +0530

    WIP: save local changes before updating

--- semaphore-support/src/App.jsx
+++ semaphore-support/src/App.jsx
@@ -1,10 +1,12 @@
 import Rules from "./components/rules/Rules";
+import Faq from "./components/FAQ/faq";
 
 const App = () => {
   return ( 
     <>
       <div className="bg-amber-300 h-screen w-screen"></div>
       <Rules/>
+      <Faq/>
       <div className="bg-amber-300 h-screen w-screen"></div>
     </>
   );
