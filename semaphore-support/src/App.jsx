fe723cbb303ff68b66b7bbd0ed37e283212d1352
Sumanth <sumanthpoojary965@gmail.com>
2025-09-05 20:49:32 +0530

    Fixed import case sensitivity for Vercel

--- semaphore-support/src/App.jsx
+++ semaphore-support/src/App.jsx
@@ -1,7 +1,6 @@
 import Rules from "./components/rules/Rules";
-import Fa from "./components/FAQ/faq";
-// import HeadsInfo from "./components/HeadsDetails/HeadsInfo";
-import HeadsInfo from "./components/HeadsInfo/HeadsInfo";
+import Faq from "./components/FAQ/faq";
+import HeadsInfo from "./components/HeadDetail/HeadsInfo";
 
 
 const App = () => {
@@ -10,7 +9,7 @@ const App = () => {
       <div className="bg-amber-300 h-screen w-screen"></div>
       <HeadsInfo />
       <Rules />
-      <Fa />
+      <Faq />
       <div className="bg-amber-300 h-screen w-screen"></div>
     </>
   );
